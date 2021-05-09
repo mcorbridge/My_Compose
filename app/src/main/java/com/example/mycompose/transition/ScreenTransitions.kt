f5a631fc0a82b6e1ae0c2d075cdd5df42dcf3733
m_cor <mikecorbridge@gmail.com>
2021-05-09 16:42:01 -0400

    working the animation

--- app/src/main/java/com/example/mycompose/transition/ScreenTransitions.kt
+++ app/src/main/java/com/example/mycompose/transition/ScreenTransitions.kt
@@ -2,9 +2,11 @@ package com.example.mycompose.transition
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import kotlinx.coroutines.delay
 
 class ScreenTransitions {
 
@@ -21,7 +23,7 @@ class ScreenTransitions {
                 ) + expandVertically(
                     expandFrom = Alignment.Top
                 ) + fadeIn(initialAlpha = 0.0f),
-                exit = slideOutVertically() + shrinkVertically() + fadeOut(),
+                exit = slideOutVertically(animationSpec = tween(250,5000,easing = FastOutSlowInEasing)) + shrinkVertically() + fadeOut(animationSpec = tween(250,5000,easing = FastOutSlowInEasing)),
                 content = content,
                 initiallyVisible = false
             )
@@ -32,13 +34,8 @@ class ScreenTransitions {
         fun SceneAnimation(content: @Composable () -> Unit) {
             AnimatedVisibility(
                 visible = true,
-                enter = slideInVertically(
-                    initialOffsetY = { -140 },
-                    animationSpec = tween(5000,0,easing = FastOutSlowInEasing)
-                ) + expandVertically(
-                    expandFrom = Alignment.Top
-                ) + fadeIn(initialAlpha = 0.0f),
-                exit = slideOutVertically(animationSpec = tween(5000,0,easing = FastOutSlowInEasing)) + shrinkVertically() + fadeOut(),
+                enter = slideInHorizontally( initialOffsetX = { -1100 }, animationSpec = tween(durationMillis = 1000, delayMillis = 100)),
+                exit = slideOutHorizontally(targetOffsetX = { 0 }, animationSpec = tween(durationMillis = 1000, delayMillis = 0)),
                 content = content,
                 initiallyVisible = false
             )
--- app/src/main/java/com/example/mycompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mycompose/ui/theme/Theme.kt
@@ -2,9 +2,11 @@ package com.example.mycompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
